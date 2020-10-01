62f61342072e8da1cb3266ac6b4c0cffeb262b13
pabloparejo <parejo.p@gmail.com>
2020-10-01 23:54:48 +0200

    Refactor click counter to hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 
-class App extends React.Component{
-  state = {
-    counter: 0
-  }
+function App() {
+  const [counter, setCounter] = React.useState(0);
 
-  handleOnClick = () => {
-    this.setState(prevState => ({ counter: prevState.counter + 1 }))
-  }
+  const handleOnClick = () => {
+    setCounter(counter + 1);
+  };
 
-  render() {
-    const { counter } = this.state;
-    return (
-      <div className="App">
-        <p>Click counter: {counter}</p>
-        <button onClick={this.handleOnClick}>Click me!</button>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <p>Click counter: {counter}</p>
+      <button onClick={handleOnClick}>Click me!</button>
+    </div>
+  );
 }
 
 export default App;
