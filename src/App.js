2ee94d846b6e6e1790012215716e6c556b766082
pabloparejo <parejo.p@gmail.com>
2020-10-01 23:52:58 +0200

    Create click counter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    counter: 0
+  }
+
+  handleOnClick = () => {
+    this.setState(prevState => ({ counter: prevState.counter + 1 }))
+  }
+
+  render() {
+    const { counter } = this.state;
+    return (
+      <div className="App">
+        <p>Click counter: {counter}</p>
+        <button onClick={this.handleOnClick}>Click me!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
