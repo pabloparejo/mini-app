00f5908680191cae6d0fca76889a925d0122d52a
pabloparejo <parejo.p@gmail.com>
2020-10-02 18:18:00 +0200

    Add antd to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <h1>Hello from Ant.d!</h1>
     </div>
   );
 }
