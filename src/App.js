c2bf928f27101042a3d888142a53b3bda54d58b0
pabloparejo <parejo.p@gmail.com>
2020-10-03 10:27:53 +0200

    Up and down

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 
-class App extends React.Component{
-  state = {
-    counter: 0
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
   }
 
-  handleOnClick = () => {
-    this.setState(prevState => ({ counter: prevState.counter + 1 }))
+  handleOnClick = offset => {
+    this.setState(prevState => ({ counter: prevState.counter + offset }));
   }
 
   render() {
     const { counter } = this.state;
     return (
       <div className="App">
-        <p>Click counter: {counter}</p>
-        <button onClick={this.handleOnClick}>Click me!</button>
+        <p>
+          Click counter:
+          {counter}
+        </p>
+        <button type="button" onClick={() => this.handleOnClick(1)}>
+          Up!
+        </button>
+        <button type="button" onClick={() => this.handleOnClick(-1)}>
+          Down!
+        </button>
       </div>
     );
   }
